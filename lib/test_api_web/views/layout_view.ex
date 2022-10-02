dd3b8af8137c8f1024c74a5dcb173c6cb4957d3d
Jinkyou Son <nallwhy@gmail.com>
2022-10-02 18:10:49 +0900

    Update phoenix_live_view

--- lib/test_api_web/views/layout_view.ex
+++ lib/test_api_web/views/layout_view.ex
@@ -1,5 +1,6 @@
 defmodule TestApiWeb.LayoutView do
   use TestApiWeb, :view
+  import Phoenix.Component
 
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
